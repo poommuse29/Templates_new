c65cd107b589b33ebde8f1fc682f555b66c594c8
Poom <poommuse29@gmail.com>
2026-08-24 09:53:01 +0700

    update from size

--- main/forms.py
+++ main/forms.py
@@ -1,21 +1,37 @@
 from django import forms
 from .models import Student
 
+
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ["prefix_name", "st_id", "fname", "lname", "major"]
         widgets = {
-            "prefix_name": forms.Select(attrs={"class": "form-select"}),
-            "st_id": forms.TextInput(attrs={"class": "form-control", "placeholder": "เช่น 640112345"}),
-            "fname": forms.TextInput(attrs={"class": "form-control", "placeholder": "เช่น สมชาย"}),
-            "lname": forms.TextInput(attrs={"class": "form-control", "placeholder": "เช่น ใจดี"}),
-            "major": forms.Select(attrs={"class": "form-select"}),
+            "prefix_name": forms.Select(attrs={"class": "form-select form-select-lg"}),
+            "st_id": forms.TextInput(
+                attrs={
+                    "class": "form-control form-control-lg",
+                    "placeholder": "เช่น 640112345",
+                }
+            ),
+            "fname": forms.TextInput(
+                attrs={
+                    "class": "form-control form-control-lg",
+                    "placeholder": "เช่น สมชาย",
+                }
+            ),
+            "lname": forms.TextInput(
+                attrs={
+                    "class": "form-control form-control-lg",
+                    "placeholder": "เช่น ใจดี",
+                }
+            ),
+            "major": forms.Select(attrs={"class": "form-select form-select-lg"}),
         }
         labels = {
-            "prefix_name": "คำนำหน้าชื่อ (Prefix)",
-            "st_id": "รหัสนักศึกษา (Student ID)",
-            "fname": "ชื่อจริง (First Name)",
-            "lname": "นามสกุล (Last Name)",
-            "major": "สาขาวิชา (Major)",
+            "prefix_name": "คำนำหน้าชื่อ ",
+            "st_id": "รหัสนักศึกษา ",
+            "fname": "ชื่อจริง ",
+            "lname": "นามสกุล ",
+            "major": "สาขาวิชา ",
         }
